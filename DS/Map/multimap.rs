45a13e8706992e23eae770c20aa7e086593f5fc1
Amit Nadiger <amith.nadiger@gmail.com>
2023-07-13 19:58:20 +0530

    Update multimap.rs

--- DS/Map/multimap.rs
+++ DS/Map/multimap.rs
@@ -29,3 +29,19 @@ Value: Value3
 Value: Value4
 
 */
+/*
+In this example, we create a HashMap called multi_map, where the keys are of type String, and the values are vectors of type Vec<String>.
+The multi_map represents a multi-map where each key can have multiple values associated with it.
+
+To insert values into the multi-map, we use the entry() method of the HashMap. 
+The entry() method returns an Entry enum, which allows us to insert values if the key doesn't exist or access the existing values if the key already exists.
+We use the or_insert() method to either insert an empty vector (Vec::new()) or retrieve the existing vector associated with the key, and then we push values into the vector.
+
+To access the values from the multi-map, we use the get() method of the HashMap. 
+It returns an Option containing a reference to the value associated with the key, which is a vector in this case.
+We can then iterate over the values and perform any desired operations.
+
+Note that in this example, we assume that each key in the multi-map can have multiple values associated with it. 
+If you need more advanced multi-map functionality or want to use a dedicated crate, you can explore third-party libraries like multimap or indexmap that provide additional features for working with multi-maps.
+
+*/
