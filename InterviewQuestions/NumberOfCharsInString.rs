3ab8235aac71aa6fb7587868d404e93b60b48afa
Amit Nadiger <amith.nadiger@gmail.com>
2023-10-01 23:10:33 +0530

    Update NumberOfCharsInString.rs

--- InterviewQuestions/NumberOfCharsInString.rs
+++ InterviewQuestions/NumberOfCharsInString.rs
@@ -1,9 +1,16 @@
 use std::collections::HashMap;
+use std::io;
 
 fn main() {
+    println!("Enter the string of u r choice");
+    let mut string_data:String = String::new();
+    std::io::stdin().read_line(&mut string_data).expect("Failed to read input");
 	let sample_string:String = String::from("Shree ram jai ram jai jai ram");
 	let map = count_chars_in_String(sample_string.as_str());
-	println!("map = {:?}",map);
+	println!("map for {} = {:?}",sample_string,map);
+	
+	let map = count_chars_in_String(string_data.as_str());
+	println!("map for {} = {:?}",string_data,map);
 }
 
 fn count_chars_in_String(data:&str) -> HashMap<char,u32>{
@@ -19,3 +26,4 @@ fn count_chars_in_String(data:&str) -> HashMap<char,u32>{
     println!("String received = {}",data);
 	map
 }
+
