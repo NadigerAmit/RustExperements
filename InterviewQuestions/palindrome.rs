d59e1d3a6f66c00fc9db3cbc341b671524848d3a
Amit Nadiger <amith.nadiger@gmail.com>
2024-04-28 14:04:12 +0530

    Update palindrome.rs

--- InterviewQuestions/palindrome.rs
+++ InterviewQuestions/palindrome.rs
@@ -1,9 +1,16 @@
+fn is_string_palindrome_efficient(input_str:&str)->bool {
+	let rev_str:String = input_str.to_lowercase().chars().rev().collect();
+	let isPalindrom = (input_str.to_lowercase() == rev_str);
+	isPalindrom
+}
+
 use std::io;
 fn main() {
 	let mut str_data:String = String::new();
 	println!("Enter the string to check for palindrome");
 	std::io::stdin().read_line(&mut str_data).expect("Invalid input data");
 	
+	//println!("Is Gadag is palindrom {}",str_data,is_string_palindrome_efficient("Gadag"));
 	println!("Is string {} palindrom {}",str_data,is_string_palindrome(&str_data.trim()));
 }
 
